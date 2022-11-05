8f13d03981c657e6c95339c2c442b8742afddaa8
AnonymousVarshney <31159138+AnonymousVarshney@users.noreply.github.com>
2022-11-05 14:41:17 +0530

    Add files via upload

--- src/test/java/com/data/parser/AppTest.java
+++ src/test/java/com/data/parser/AppTest.java
@@ -1,6 +1,13 @@
 package com.data.parser;
 
+import com.data.parser.impl.CsvFileParser;
+import com.data.parser.impl.JsonFileParser;
+import com.data.parser.interfaces.DataParserInterface;
+import com.data.parser.vo.OutputDataVo;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 /**
  * Unit test.
@@ -8,7 +15,25 @@ import org.junit.Test;
 public class AppTest
 {
     @Test
-    public void testMain() {
+    public void testCsv() {
+          DataParserInterface csvfileParser=new CsvFileParser("orders.csv");
+        List<OutputDataVo> outputDataVoList =csvfileParser.parseFileAndConvertToVo();
+
+        Assert.assertEquals(outputDataVoList.get(0).getResult(),"OK");
+        Assert.assertEquals(outputDataVoList.get(1).getResult(),"OK");
+        Assert.assertEquals(outputDataVoList.get(2).getResult(),"error");
+
+
+
+    }
+
+    @Test
+    public void testJson() {
+        DataParserInterface csvfileParser=new JsonFileParser("orders.json");
+        List<OutputDataVo> outputDataVoList =csvfileParser.parseFileAndConvertToVo();
+
+        Assert.assertEquals(outputDataVoList.get(0).getResult(),"error");
+
 
 
     }
